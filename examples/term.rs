47b49b6ed4544d4d104563a0c59980e3889f390d
Armin Ronacher <armin.ronacher@active-4.com>
2017-05-09 14:06:37 +0200

    Added a term example

---
+++ examples/term.rs
@@ -0,0 +1,27 @@
+extern crate console;
+
+use std::io;
+use std::time::Duration;
+use std::thread;
+
+use console::{Term, style};
+
+
+fn do_stuff() -> io::Result<()> {
+    let term = Term::stdout();
+    term.write_line("Going to do some counting now")?;
+    for x in 0..10 {
+        if x != 0 {
+            term.move_cursor_up(1)?;
+        }
+        term.write_line(&format!("Counting {}/10", style(x + 1).cyan()))?;
+        thread::sleep(Duration::from_millis(400));
+    }
+    term.clear_last_lines(1)?;
+    term.write_line("Done counting!")?;
+    Ok(())
+}
+
+fn main() {
+    do_stuff().unwrap();
+}
