db2ac8968eb8d20e598376b59d4a97db714d2fe3
Armin Ronacher <armin.ronacher@active-4.com>
2017-05-11 07:32:23 +0200

    Fixed escape key detection on unix

--- src/unix_term.rs
+++ src/unix_term.rs
@@ -64,7 +64,7 @@ pub fn key_from_escape_codes(buf: &[u8]) -> Key {
         b"\x1b[A" => Key::ArrowUp,
         b"\x1b[B" => Key::ArrowDown,
         b"\n" | b"\r" => Key::Enter,
-        b"\x1b[" => Key::Escape,
+        b"\x1b" => Key::Escape,
         buf => {
             if let Ok(s) = str::from_utf8(buf) {
                 if let Some(c) = s.chars().next() {
