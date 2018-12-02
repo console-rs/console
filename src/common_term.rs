2915d6ad23b2ed1dc3ebd2f84abc3511337e044b
Armin Ronacher <armin.ronacher@active-4.com>
2018-12-02 21:02:54 +0100

    Added missing module

---
+++ src/common_term.rs
@@ -0,0 +1,24 @@
+use std::io;
+
+use term::Term;
+
+pub fn move_cursor_down(out: &Term, n: usize) -> io::Result<()> {
+    if n > 0 {
+        out.write_str(&format!("\x1b[{}B", n))
+    } else {
+        Ok(())
+    }
+}
+
+pub fn move_cursor_up(out: &Term, n: usize) -> io::Result<()> {
+    if n > 0 {
+        out.write_str(&format!("\x1b[{}A", n))
+    } else {
+        Ok(())
+    }
+}
+
+pub fn clear_line(out: &Term) -> io::Result<()> {
+    out.write_str("\r\x1b[2K")
+}
+
