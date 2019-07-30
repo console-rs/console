2d8cf069e4bbe1ed7f434ce7447e17961d7dee43
Devin R <devin.ragotzy@gmail.com>
2019-07-30 07:29:12 -0400

    fix my import

--- src/windows_term.rs
+++ src/windows_term.rs
@@ -24,7 +24,7 @@ use winapi::um::wincon::{
 use winapi::um::winnt::{CHAR, HANDLE, INT, WCHAR};
 
 use atty;
-pub use common_term::*;
+pub use common_term::{self, *};
 use kb::Key;
 use term::{Term, TermTarget};
 
