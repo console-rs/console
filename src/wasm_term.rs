ee611a4014112fa0468866e1aefde0ee950e6e7a
Armin Ronacher <armin.ronacher@active-4.com>
2020-01-25 19:17:36 +0100

    Add missing wasm file

---
+++ src/wasm_term.rs
@@ -0,0 +1,38 @@
+use std::fmt::Display;
+use std::io;
+
+use crate::kb::Key;
+use crate::term::Term;
+
+pub use crate::common_term::*;
+
+pub const DEFAULT_WIDTH: u16 = 80;
+
+#[inline]
+pub fn is_a_terminal(_out: &Term) -> bool {
+    false
+}
+
+pub fn terminal_size() -> Option<(u16, u16)> {
+    None
+}
+
+pub fn read_secure() -> io::Result<String> {
+    Err(io::Error::new(
+        io::ErrorKind::Other,
+        "unsupported operation",
+    ))
+}
+
+pub fn read_single_key() -> io::Result<Key> {
+    Err(io::Error::new(
+        io::ErrorKind::Other,
+        "unsupported operation",
+    ))
+}
+
+pub fn wants_emoji() -> bool {
+    false
+}
+
+pub fn set_title<T: Display>(_title: T) {}
