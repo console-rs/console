0a2c4ce4195b1f5e70375571711ff4f5addd5ce2
Mike Grunweg <grunweg@posteo.de>
2022-04-29 22:02:01 +0200

    Make sure clear_last_lines never clears below the current cursor.
    Instead, check the number of rows above the cursor position,
    and error if passed a too large number.

--- src/term.rs
+++ src/term.rs
@@ -434,14 +434,16 @@ impl Term {
         clear_line(self)
     }
 
-    /// Clear the last `n` lines before the current line.
+    /// Clear the last `n` lines before the current line, if possible.
     ///
     /// Position the cursor at the beginning of the first line that was cleared.
-    ///
-    /// **Caution.** When `n` is larger than the number of lines above the
-    /// current cursor, the top `n` lines are cleared --- including some lines
-    /// below the cursor.
+    /// Error when `n` is larger than the number of lines before the current cursor.
     pub fn clear_last_lines(&self, n: usize) -> io::Result<()> {
+        let (current_row, _) = get_cursor_position(self)?;
+        if usize::from(current_row) < n {
+            // We cannot move up n lines, only current_row ones.
+            return Err(io::Error::new(io::ErrorKind::Other, format!("can only move up {} lines, not {}", current_row, n)));
+        }
         self.move_cursor_up(n)?;
         for _ in 0..n {
             self.clear_line()?;
