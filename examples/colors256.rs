55b043e746745ddf394295f7153dfa56cb6d548e
danieleades <33452915+danieleades@users.noreply.github.com>
2022-12-07 09:07:19 +0000

    run clippy on all targets (#141)

--- examples/colors256.rs
+++ examples/colors256.rs
@@ -4,14 +4,14 @@ fn main() {
     for i in 0..=255 {
         print!("{:03} ", style(i).color256(i));
         if i % 16 == 15 {
-            println!("");
+            println!();
         }
     }
 
     for i in 0..=255 {
         print!("{:03} ", style(i).black().on_color256(i));
         if i % 16 == 15 {
-            println!("");
+            println!();
         }
     }
 }
