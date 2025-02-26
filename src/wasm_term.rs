fe15300867049705114fa07020725b07ae2a708c
Dirkjan Ochtman <dirkjan@ochtman.nl>
2025-02-26 11:45:33 +0100

    Fix unreachable_pub warnings on Windows and WASM

--- src/wasm_term.rs
+++ src/wasm_term.rs
@@ -4,12 +4,12 @@ use std::io;
 use crate::kb::Key;
 use crate::term::Term;
 
-pub use crate::common_term::*;
+pub(crate) use crate::common_term::*;
 
-pub const DEFAULT_WIDTH: u16 = 80;
+pub(crate) const DEFAULT_WIDTH: u16 = 80;
 
 #[inline]
-pub fn is_a_terminal(_out: &Term) -> bool {
+pub(crate) fn is_a_terminal(_out: &Term) -> bool {
     #[cfg(target = "wasm32-wasip1")]
     {
         unsafe { libc::isatty(out.as_raw_fd()) != 0 }
@@ -21,25 +21,25 @@ pub fn is_a_terminal(_out: &Term) -> bool {
 }
 
 #[inline]
-pub fn is_a_color_terminal(_out: &Term) -> bool {
+pub(crate) fn is_a_color_terminal(_out: &Term) -> bool {
     // We currently never report color terminals.  For discussion see
     // the issue in the WASI repo: https://github.com/WebAssembly/WASI/issues/162
     false
 }
 
 #[inline]
-pub fn terminal_size(_out: &Term) -> Option<(u16, u16)> {
+pub(crate) fn terminal_size(_out: &Term) -> Option<(u16, u16)> {
     None
 }
 
-pub fn read_secure() -> io::Result<String> {
+pub(crate) fn read_secure() -> io::Result<String> {
     Err(io::Error::new(
         io::ErrorKind::Other,
         "unsupported operation",
     ))
 }
 
-pub fn read_single_key(_ctrlc_key: bool) -> io::Result<Key> {
+pub(crate) fn read_single_key(_ctrlc_key: bool) -> io::Result<Key> {
     Err(io::Error::new(
         io::ErrorKind::Other,
         "unsupported operation",
@@ -47,8 +47,8 @@ pub fn read_single_key(_ctrlc_key: bool) -> io::Result<Key> {
 }
 
 #[inline]
-pub fn wants_emoji() -> bool {
+pub(crate) fn wants_emoji() -> bool {
     false
 }
 
-pub fn set_title<T: Display>(_title: T) {}
+pub(crate) fn set_title<T: Display>(_title: T) {}
